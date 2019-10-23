a86ec40e212d5079ad5c8863c621aecfc4631611
Cole Holmes <mitchellcholmes@gmail.com>
2019-10-22 22:54:07 -0500

    initial

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import { useScreens } from 'react-native-screens';
+
+import MealsNavigator from './navigation/MealsNavigator'
+
+//possibly improves efficiency for the ios or android platforms
+useScreens();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
+
+export default function App() {
+  const [fontLoaded, setFontLoaded]= useState(false);
+
+  if (!fontLoaded) {
+    <AppLoading
+    startAsync= {fetchFonts}
+    onFinish={() => setFontLoaded(true)}
+    />
+  }
+
+  return (
+    <MealsNavigator/>
+  );
+}
+
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile'
+
+import { CATEGORIES } from '../data/dummy-data';
+
+
+const CategoriesScreen = props => {
+
+  const renderGridItem = (itemData) => {
+    return ( 
+      <CategoryGridTile 
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={()=>{
+          props.navigation.navigate({
+            routeName: 'CategoryMeals', 
+            params: {
+              categoryId: itemData.item.id
+            }
+          });
+        }}
+      />
+    )
+  }
+
+  return (
+    <FlatList keyExtractor={(item, index)=> item.id} data={CATEGORIES} numColumns={2} renderItem={renderGridItem} />
+  )
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoriesScreen;
